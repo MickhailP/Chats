9516bb67f2449f8a15e31897bcc15495697fb5f0
Mikhail <mich_p@inbox.ru>
2023-06-08 13:04:02 +0300

    Initial commit

---
+++ Chats/ChatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatsApp.swift
+//  Chats
+//
+//  Created by Миша Перевозчиков on 08.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct ChatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
