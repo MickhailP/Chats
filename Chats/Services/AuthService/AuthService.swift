66db5191a46e3ca251ff29265df2bb1cc898a2e9
Mikhail <mich_p@inbox.ru>
2023-06-08 22:46:38 +0300

    Добавлен AuthService под управлением AuthenticationProtocol
    Реализоавн метод  func verify(phoneNumber: String) throws

---
+++ Chats/Services/AuthService/AuthService.swift
@@ -0,0 +1,80 @@
+//
+//  AuthService.swift
+//  Chats
+//
+//  Created by Миша Перевозчиков on 08.06.2023.
+//
+
+import Foundation
+import Combine
+
+
+protocol AuthenticationProtocol: AnyObject & ObservableObject {
+	 var networkingService: NetworkProtocol { get }
+
+	 func verify(phoneNumber: String) throws
+}
+
+
+
+final class AuthService: AuthenticationProtocol {
+
+	 let networkingService: NetworkProtocol
+
+	 @Published var verificationCode: Int?
+
+
+	 init(networkingService: NetworkProtocol) {
+		  self.networkingService = networkingService
+	 }
+
+
+	 func verify(phoneNumber: String) throws {
+
+		  guard let url = URL(string: "https://plannerok.ru/api/v1/users/send-auth-code/") else {
+				throw ErrorMessage.badURl
+		  }
+
+		  let json = """
+		  {
+		  "phone": "\(phoneNumber)"
+		  }
+		  """
+
+		  guard let request = configureTokenFreeRequest(httpMethod: "POST", url: url, json: json) else {
+				throw ErrorMessage.barRequest
+		  }
+
+
+		  Task {
+				let result = await networkingService.downloadDataResult(for: request)
+
+				switch result {
+					 case .success(let data):
+						  if let decoded: VerificationCode = Decoder.decode(data),
+							  decoded.isSuccess == true {
+								await MainActor.run {
+									 verificationCode = 133337
+								}
+						  }
+					 case .failure(let failure):
+						  throw failure
+				}
+		  }
+	 }
+
+
+
+	 private func configureTokenFreeRequest(httpMethod: String, url: URL, json: String ) -> URLRequest? {
+
+		  var request = URLRequest(url: url)
+		  request.httpMethod = httpMethod
+		  request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+
+		  if let uploadData = json.data(using: .utf8)  {
+				request.httpBody = uploadData
+				return request
+		  }
+		  return nil
+	 }
+}
