0a9a6cc277f5462c0aef9482553cfe653e297975
Mikhail <mich_p@inbox.ru>
2023-06-09 15:07:58 +0300

    Корректировка AuthData в optional type

--- Chats/Models/AuthData/AuthData.swift
+++ Chats/Models/AuthData/AuthData.swift
@@ -8,8 +8,14 @@
 import Foundation
 
 struct AuthData: Codable {
-	 let refreshToken: String
-	 let accessToken: String
-	 let userID: Int
+	 let refreshToken, accessToken: String?
+	 let userID: Int?	
 	 let isUserExists: Bool?
+
+	 enum CodingKeys: String, CodingKey {
+		  case refreshToken
+		  case accessToken
+		  case userID
+		  case isUserExists
+	 }
 }
