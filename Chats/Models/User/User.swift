c52546a78060858ab92255257c2e6ca4beed1d2a
Mikhail <mich_p@inbox.ru>
2023-06-13 21:56:01 +0300

    Дополнительные поля в модели User

--- Chats/Models/User/User.swift
+++ Chats/Models/User/User.swift
@@ -11,4 +11,18 @@ import Foundation
 struct User: Codable {
 
 	 let phone, name, username: String
+
+	 let birthday, city: String
+	 let vk, instagram: String
+	 let status, avatar: String
+	 let id: Int
+	 let last: String
+	 let online: Bool
+	 let created: String
+	 let completedTask: Int
+	 let avatars: Avatars
+}
+
+struct Avatars: Codable {
+	 let avatar, bigAvatar, miniAvatar: String
 }
