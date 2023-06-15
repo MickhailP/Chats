515183c46a5ed41daf36e5342ce52d312cc97846
Mikhail <mich_p@inbox.ru>
2023-06-15 21:51:37 +0300

    Изменен AvatarImageView для возможности создания с помощью base64

--- Chats/CustomViews/ImageView/AvatarImageView.swift
+++ Chats/CustomViews/ImageView/AvatarImageView.swift
@@ -9,8 +9,21 @@ import SwiftUI
 
 struct AvatarImageView: View {
 
-	 let avatarURL: String?
+	 let uiImage: UIImage?
+	 let base64: String?
 	 let online: Bool?
+
+	 init(uiImage: UIImage?, online: Bool?) {
+		  self.uiImage = uiImage
+		  self.online = online
+		  self.base64 = nil
+	 }
+
+	 init(base64: String?, online: Bool?) {
+		  self.base64 = base64
+		  self.online = online
+		  self.uiImage = ImageConverter().convertBase64ToImage(base64String: base64)
+	 }
 
 	 var body: some View {
 		  ZStack {
@@ -20,27 +33,44 @@ struct AvatarImageView: View {
 				Circle()
 					 .strokeBorder(.white, lineWidth: 3)
 
-
-				ImageView(imageUrl: avatarURL)
-					 .frame(maxWidth: 100)
+				imageView
 					 .clipShape(Circle())
 					 .padding(3)
 
 					 .overlay{
-						  Circle()
-								.fill( online ?? false ? .green : .red)
-								.frame(maxWidth: 15)
-								.offset(x: 35, y: 35)
-								.shadow(radius: 10)
-
+						  if let online {
+								Circle()
+									 .fill( online ? .green : .red)
+									 .frame(maxWidth: 15)
+									 .offset(x: 35, y: 35)
+									 .shadow(radius: 10)
+						  }
 					 }
+
 		  }
 		  .frame(maxWidth: 100, maxHeight: 100)
 	 }
 }
 
+extension AvatarImageView {
+
+	 @ViewBuilder var imageView: some View {
+		  if let uiImage {
+				Image(uiImage: uiImage)
+					 .resizable()
+					 .scaledToFill()
+		  } else {
+				Image(systemName: "person")
+					 .resizable()
+					 .scaledToFit()
+					 .padding()
+		  }
+	 }
+}
+
+
 struct AvatarImageView_Previews: PreviewProvider {
 	 static var previews: some View {
-		  AvatarImageView(avatarURL: User.example.avatar, online: User.example.online)
+		  AvatarImageView(uiImage: ImageConverter().convertBase64ToImage(base64String: User.example.avatar ?? ""), online: User.example.online)
 	 }
 }
--- Chats/Services/ImageConverter/ImageConverter.swift
+++ Chats/Services/ImageConverter/ImageConverter.swift
@@ -1,4 +1,4 @@
-Соз//
+//
 //  ImageConverter.swift
 //  Chats
 //
